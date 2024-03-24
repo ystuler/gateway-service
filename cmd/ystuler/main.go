ec722b1c3337b01dd6952b55aa23d9820c7d4c29
Dmitriy Lapin <dima.ladmol@yandex.ru>
2024-03-24 14:32:36 +0300

    feat: add file structure

---
+++ cmd/ystuler/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	http.ListenAndServe(":3000", r)
+}
